13086c7a26cc2653b0baf44928a6d8693c11bc60
Snegirev Anfim <anfim.sn@gmail.com>
2023-03-05 15:56:16 +0400

    Create react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { createTheme } from "@mui/material";
 
-function App() {
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+const GlobalStyled = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  h1, h2, h3, h4, h5, h6, p {
+    font-family: "Roboto", sans-serif;
+  }
+
+  body {
+    background: #282c34;
+  }
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyled />
+      <div className="App">
+        <header className="App-header">
+          <h1>Armenian PDD</h1>
+        </header>
+      </div>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
